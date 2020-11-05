5095c453bfcceb7b76d8ab49575e02b461dff8d4
Maxime RIO <maxime.rio@niwa.co.nz>
2020-11-05 01:41:19 +0000

    use paired files in jupytext, add Auckland and CBD

--- notebooks/01_explore_cas.py
+++ notebooks/01_explore_cas.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: crash_prediction
+#     language: python
+#     name: crash_prediction
+# ---
+
 # # Exploration of CAS (Crash analysis system) data
 
 from pathlib import Path
@@ -27,12 +43,12 @@ if not dset_path.exists():
 dset = pd.read_csv(dset_path)
 dset
 
-dset.info()
-
 # The dataset contains 72 columns, describing various aspects of the recorded
 # car crashes. The full description of the fields is available online, see
 # https://opendata-nzta.opendata.arcgis.com/pages/cas-data-field-descriptions.
-#
+
+dset.columns
+
 # Note that `X` and `Y` are geographical coordinates using NZTM2000 (New Zealand
 # Transverse Mercator 2000) coordinate system (see [EPSG:2193](https://epsg.io/2193)).
 
@@ -41,40 +57,34 @@ dset.info()
 hv_map = dset.hvplot.points(
     "X",
     "Y",
-    geo=True,
     tiles="OSM",
     crs=2193,
-    frame_width=600,
+    frame_width=400,
     datashade=True,
-    dynspread=True,
-    # interactive=False,
 )
-hv_auckland = hv_map.redim.range(X=(1739669.89, 1781221.41), Y=(5932097.90, 5901931.60))
+hv_map
 
-hv_map + hv_auckland
-
-# some test using Dask to accelerate rendering
-
-from distributed import Client
-
-client = Client()
-client
-
-import dask.dataframe as dd
-import hvplot.dask  # noqa
-
-dset2 = dd.from_pandas(dset, chunksize=300).persist()
-
-dset2.hvplot.points(
+hv_auckland = dset.hvplot.points(
     "X",
     "Y",
-    geo=True,
     tiles="OSM",
     crs=2193,
-    frame_width=600,
-    datashade=True
+    frame_width=400,
+    datashade=True,
 )
+hv_auckland.redim.range(X=(1739669.89, 1781221.41), Y=(5932097.90, 5901931.60))
 
+hv_cbd = dset.hvplot.points(
+    "X",
+    "Y",
+    tiles="OSM",
+    crs=2193,
+    frame_width=400,
+    datashade=True,
+)
+hv_cbd.redim.range(X=(1755876.21,  1758568.09), Y=(5921526.71, 5918933.89))
+
+# +
 # import geopandas as gpd
 #
 # gdset = gpd.GeoDataFrame(
